5693f9a08199ed3d722ae263edbc6da9cc422bac
LovroMagdic <lovromagdic23@gmail.com>
2024-11-16 20:01:17 +0100

    tesseract anno

--- QA test.py
+++ QA test.py
@@ -1,10 +1,18 @@
 import cv2 
 import pytesseract
+import os
+import time
 
 # this could be used for QA of annotated images
 
-image = "3D modeling and simulation.png"
+label = open("labels_tesseract.csv", "a")
+
+path = ".CNN/dataset"
+files = os.listdir(path)
+for file in files:
+    print(file)
+    time.sleep(10000)
 pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/Cellar/tesseract/5.3.0_1/bin/tesseract'
-string = pytesseract.image_to_string(image)
+string = pytesseract.image_to_string(file)
 
 print(string)
